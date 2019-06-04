4510936f7550966b207e886837c2aabdada35c70
Omer Ben-Amram <omerbenamram@gmail.com>
2019-06-04 16:35:30 +0300

    update mft

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ install_requires = ["setuptools-rust"]
 
 setup(
     name="mft",
-    version="0.2.0",
+    version="0.4.1",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Development Status :: 3 - Alpha",
